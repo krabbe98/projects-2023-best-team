27025303e78fd539b66eaeced52bc67b5fbf46e8
lj-username <shk618@alumni.ku.dk>
2023-05-17 14:12:56 +0200

    Most recent version

--- modelproject/stackelberg.py
+++ modelproject/stackelberg.py
@@ -28,11 +28,12 @@ class StackelbergSolver:
         return self.demand_function(q1,q2)*q1-self.cost_function(q1,c)
     
     def reaction(self, q2,c1):
-        # Maaaaaaax profit
+        # Maximizing profit
         
-        responce =  optimize.minimize(lambda x: - self.profits(c1,x,q2), x0=0, method = 'SLSQP')
-        return responce.x # best responce
+        response =  optimize.minimize(lambda x: - self.profits(c1,x,q2), x0=0, method = 'SLSQP')
+        return np.maximum(0, response.x) # best response
 
+#can fixed point be deleted?
     def fixed_point(self, q):
         par = self.par
         # the fixed point is the q that equals the reaction.
@@ -43,7 +44,7 @@ class StackelbergSolver:
         par = self.par 
         initial_guess = np.array([0])
         # solve system of equations.
-        res = optimize.minimize(lambda q1: -self.profits( par.c[0], q1,self.reaction(q1 , par.c[0]) ), initial_guess)
+        res = optimize.minimize(lambda q1: -np.maximum(self.profits( par.c[0], q1,self.reaction(q1 , par.c[0])), 0) , initial_guess)
         q1 =res.x
         q2 = self.reaction(q1,par.c[1])
         return q1, q2
@@ -88,13 +89,6 @@ def plot_stackelberg_interact():
 
 
 
-
-from scipy import optimize
-import numpy as np
-import matplotlib.pyplot as plt
-from types import SimpleNamespace
-import ipywidgets as widgets
-
 class StackelbergSolver2:
     def __init__(self):
         """ setup model """
@@ -126,8 +120,8 @@ class StackelbergSolver2:
     def reaction(self, q2,c1):
         # Maaaaaaax profit
         
-        responce =  optimize.minimize(lambda x: - self.profits2(c1,x,q2), x0=0, method = 'SLSQP')
-        return responce.x # best responce
+        response =  optimize.minimize(lambda x: - self.profits2(c1,x,q2), x0=0, method = 'SLSQP')
+        return response.x # best response
 
     def fixed_point(self, q):
         par = self.par
@@ -143,3 +137,42 @@ class StackelbergSolver2:
         q1 =res.x
         q2 = self.reaction(q1,par.c[1])
         return q1, q2
+    
+def plot_stackelberg2(a=1, b=1, X=0, c1=0, c2=0, t1=0, t2=0.1):
+    model = StackelbergSolver2()
+    # Update model parameters
+    model.par.a = a
+    model.par.b = b
+    model.par.X = X
+    model.par.c = [c1, c2]
+    model.par.t = [t1,t2]    
+    # x axis grid
+    range_q1 = np.arange(0,21,0.1)
+    # y axis grid
+    range_q2 = np.zeros((range_q1.size,))
+    
+    for it, q1 in enumerate(range_q1):
+        # find equilibrium quantities
+        range_q2[it] = model.reaction(q1, c1)
+    
+    fig, ax = plt.subplots()
+    ax.plot(range_q1, range_q2, label='Company 2')
+    ax.set_xlabel('$q_1$')
+    ax.set_ylabel('$q_2$')
+    ax.set_title('Stackelberg Equilibrium Quantities')
+    ax.legend()
+    plt.show()
+
+# Set up interactive sliders
+def plot_stackelberg_interact2():
+    widgets.interact(
+        plot_stackelberg,
+        a=widgets.FloatSlider(description="a", min=1, max=5, step=0.25, value=1),
+        b=widgets.FloatSlider(description="b", min=1, max=5, step=0.25, value=1),
+        X=widgets.FloatSlider(description="X", min=1, max=50, step=0.5, value=20),
+        c1=widgets.FloatSlider(description="c1", min=0, max=5, step=0.1, value=0),
+        c2=widgets.FloatSlider(description="c2", min=0, max=5, step=0.1, value=0),
+        t2=widgets.FloatSlider(description="t2", min=0, max=1, step=0.1, value=0)
+);
+
+    
